bbf03e13dc370020418f53a1d8278eff82eccc0e
Selvaragavan_K <146157252+Selvaragavan17@users.noreply.github.com>
2025-08-11 23:34:15 +0530

    Update Sequencer.sv

--- ALU/Sequencer.sv
+++ ALU/Sequencer.sv
@@ -1,7 +1,8 @@
+
 class alu_sequencer extends uvm_sequencer#(seq_item);
-  `uvm_component_utils(alu_sequencer);
+  `uvm_component_utils(alu_sequencer)
   
-  function new(string name="alu_sequencer",uvm_component parent=null);
-    super.new(name,parent);
+  function new(string name = "alu_sequencer", uvm_component parent = null);
+    super.new(name, parent);
   endfunction
 endclass
