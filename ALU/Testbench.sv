1e1457b7426234566847d374e3c618485315f977
Selvaragavan_K <146157252+Selvaragavan17@users.noreply.github.com>
2025-08-11 23:34:51 +0530

    Update Testbench.sv

--- ALU/Testbench.sv
+++ ALU/Testbench.sv
@@ -1,5 +1,6 @@
 `include "uvm_macros.svh"
 import uvm_pkg::*;
+
 `include "interface.sv"
 `include "test.sv"
 
@@ -7,28 +8,37 @@ module tp_top;
   bit clk;
   bit rst;
   
-  intf intff(clk);
+  // Instantiate interface
+  intf intff(clk, rst);
+
+  // Instantiate DUT
   ALU uut(
-    .clk(intff.clk);
-    .rst(intff.rst);
-    .a(intff.a);
-    .b(intff.b);
-    .sel(intff.sel);
-    .result(intff.result));
+    .clk(intff.clk),
+    .rst(intff.rst),
+    .a(intff.a),
+    .b(intff.b),
+    .sel(intff.sel),
+    .result(intff.result)
+  );
   
-  always #5 clk=~clk;
+  // Clock generation
+  initial clk = 0;
+  always #5 clk = ~clk;
   
+  // Reset logic
+  initial begin
+    rst = 1;
+    #20 rst = 0;
+  end
+
+  // Pass interface handle to UVM testbench
+  initial begin
+    uvm_config_db#(virtual intf)::set(null, "*", "intff", intff);
+  end
+
+  // Run UVM test
   initial begin
     run_test("test");
-    #5000;
-    $finish;
   end
-  
-  initial begin
-    rst=0;
-  end
-  
-  initial begin
-    uvm_config_db#(virtual intf)::set(null,"","intff",intff);
-  end
-    
+
+endmodule
