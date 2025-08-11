ecfc599c995c994962fcac89b76f4b770eff6b72
Selvaragavan_K <146157252+Selvaragavan17@users.noreply.github.com>
2025-08-11 23:31:52 +0530

    Update Driver.sv

--- ALU/Driver.sv
+++ ALU/Driver.sv
@@ -1,28 +1,33 @@
 class alu_driver extends uvm_driver#(seq_item);
-  `uvm_components_utils(alu_driver)
+  `uvm_component_utils(alu_driver)
+
   virtual intf intff;
   seq_item seq;
-  
-  function new(string name="alu_driver",uvm_component parent=null);
-    super.new(name,parent);
+
+  function new(string name = "alu_driver", uvm_component parent = null);
+    super.new(name, parent);
   endfunction
-  
+
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if(!uvm_connfig_db#(virtual intf)::get(this,"*","intff",intff))
-      `uvm_fatal("DRIVER","NO DATA IN THE DRIVER");
+    if (!uvm_config_db#(virtual intf)::get(this, "*", "intff", intff))
+      `uvm_fatal("DRIVER", "NO DATA IN THE DRIVER");
   endfunction
-  
+
   task run_phase(uvm_phase phase);
     forever begin
-      
+      seq = seq_item::type_id::create("seq");
       seq_item_port.get_next_item(seq);
-      intff.a=seq.a;
-      intff.b=seq.b;
-      intff.sel=seq.sel;
-      intff.result=seq.result;
-      `uvm_info("DRIVER","A=%d,B=%d,SEL=%d,RESULT=%D",intff.a,inntff.b,intff.sel,intff.result);
-      seq_item_port.item_done(seq);
+
+      intff.a     = seq.a;
+      intff.b     = seq.b;
+      intff.sel   = seq.sel;
+      intff.result= seq.result;
+
+      `uvm_info("DRIVER", $sformatf("A=%0d, B=%0d, SEL=%0d, RESULT=%0d",
+                 intff.a, intff.b, intff.sel, intff.result), UVM_MEDIUM)
+
+      seq_item_port.item_done();
     end
   endtask
 endclass
