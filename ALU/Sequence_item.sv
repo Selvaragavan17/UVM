a88a8e8628008425fbe9833ad71f9065134f6eae
Selvaragavan_K <146157252+Selvaragavan17@users.noreply.github.com>
2025-08-11 23:33:57 +0530

    Update Sequence_item.sv

--- ALU/Sequence_item.sv
+++ ALU/Sequence_item.sv
@@ -1,13 +1,14 @@
+
 class seq_item extends uvm_sequence_item;
-  rand bit [3:0]a;
-  rand bit [3:0]b;
-  rand bit [2:0]sel;
-  rand bit [5:0]result;
+  rand bit [3:0] a;
+  rand bit [3:0] b;
+  rand bit [2:0] sel;
+  rand bit [5:0] result;
   
-  `uvm_objects_utils(seq_item);
+  `uvm_object_utils(seq_item)
   
-  function new (string name="seq_item");
+  function new(string name = "seq_item");
     super.new(name);
   endfunction
-  
 endclass
+
