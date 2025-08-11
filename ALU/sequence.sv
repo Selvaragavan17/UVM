72f29b20aa03927d4008f9acf946c630ee56c0ef
Selvaragavan_K <146157252+Selvaragavan17@users.noreply.github.com>
2025-08-11 22:07:01 +0530

    Create sequence.sv

---
+++ ALU/sequence.sv
@@ -0,0 +1,14 @@
+class alu_sequence extends uvm_sequence#(seq_item);
+  `uvm_object_utils(alu_sequence);
+  
+  function new(string name="alu_sequence");
+    super.new(name);
+  endfunction
+  
+  task body()
+    seq_item seq;
+    seq=seq_item::type_id::create(seq);
+    
+    `uvm_do(seq);
+  endtask
+endclass
