386d85e802dcf5e3c70cb4ebf6a95e0818f9b6c9
Selvaragavan_K <146157252+Selvaragavan17@users.noreply.github.com>
2025-08-11 23:32:14 +0530

    Update Environment.sv

--- ALU/Environment.sv
+++ ALU/Environment.sv
@@ -1,20 +1,22 @@
-class alu_environment extends uvm_environment;
-  `uvm_components_utils(alu_environment)
-  agent agt;
-  scoreboard scr;
+
+class alu_environment extends uvm_env;
+  `uvm_component_utils(alu_environment)
+
+  alu_agent agt;
+  alu_scoreboard scr;
   
-  function new(string name="alu_environment",uvm_component parent=null);
-    super.new(name,parent);
+  function new(string name = "alu_environment", uvm_component parent = null);
+    super.new(name, parent);
   endfunction
   
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    agt=alu_agent::type_id::create("agt",this);
-    scr=alu_scoreboard::type_id::create("scr",this);
+    agt = alu_agent::type_id::create("agt", this);
+    scr = alu_scoreboard::type_id::create("scr", this);
   endfunction
   
-  function void connect(uvm_phase phase);
-    super.connect(phase);
-    agt.mon1.item_collected_port.connect(scr.item_collected_export);
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    agt.mon1.item_collected_port.connect(scr.item_collected_import);
   endfunction
 endclass
