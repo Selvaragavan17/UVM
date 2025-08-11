a178dd4746d207e59b55c9c8cbd7e2d3007d15d8
Selvaragavan_K <146157252+Selvaragavan17@users.noreply.github.com>
2025-08-11 23:34:32 +0530

    Update Test.sv

--- ALU/Test.sv
+++ ALU/Test.sv
@@ -8,29 +8,28 @@
 `include "environment.sv"
 
 class alu_test extends uvm_test;
-  `uvm_component_utils(test);
-  
+  `uvm_component_utils(alu_test)   
+
   alu_environment env;
   alu_sequence seq2;
-  
-  function new(string name="alu_test",uvm_component parent=null);
-    super.new(name,parent);
+
+  function new(string name = "alu_test", uvm_component parent = null);
+    super.new(name, parent);
   endfunction
-  
+
   function void build_phase(uvm_phase phase);
-    super.new(phase);
-    env=alu_environment::type_id::create("env",this);
-    seq2=alu_sequence::type_id::create("seq2",this);
+    super.build_phase(phase);   
+    env  = alu_environment::type_id::create("env", this);
+    seq2 = alu_sequence::type_id::create("seq2", this);
   endfunction
-  
-  task run_phase(uvm_component phase);
-    repeat(10)
-      phase.raise_objection(this);
+
+  task run_phase(uvm_phase phase);   
+    phase.raise_objection(this);     
+
+    repeat (10) begin
       seq2.start(env.agt.seqr);
-      phase.drop_objection(this);
+    end
+
+    phase.drop_objection(this);      
   endtask
 endclass
-  
-  
-  
-  
