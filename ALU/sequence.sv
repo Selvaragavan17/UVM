8ee220c446012efa6c6ead530144244b434928e5
Selvaragavan_K <146157252+Selvaragavan17@users.noreply.github.com>
2025-08-11 23:35:10 +0530

    Update sequence.sv

--- ALU/sequence.sv
+++ ALU/sequence.sv
@@ -1,14 +1,16 @@
+
+
 class alu_sequence extends uvm_sequence#(seq_item);
-  `uvm_object_utils(alu_sequence);
+  `uvm_object_utils(alu_sequence)
   
-  function new(string name="alu_sequence");
+  function new(string name = "alu_sequence");
     super.new(name);
   endfunction
   
-  task body()
+  task body();
     seq_item seq;
-    seq=seq_item::type_id::create(seq);
+    seq = seq_item::type_id::create("seq");
     
-    `uvm_do(seq);
+    `uvm_do(seq)
   endtask
 endclass
